64b081c2db4b0e434d5919204104eb781c1af52c
mattschreiber <matt.schreiber01@gmail.com>
2021-04-10 15:07:10 -0400

    added daily score to view

--- src/components/RecordEntry.js
+++ src/components/RecordEntry.js
@@ -1,5 +1,7 @@
 import React, {useState} from 'react';
 
+import DailyScore from './DailyScore';
+
 function RecordEntry() {
     const [period, setPeriod] = useState('');
     const [team1, setTeam1] = useState('Naps');
@@ -9,21 +11,33 @@ function RecordEntry() {
 
     const [teamList] = useState(['Ironmen', 'Naps', 'Row']);
 
+    const [scoreObj] = useState([
+        {period: 1, team1: "Naps", score1: 50, team2: "Durham Bulls", score2: 40},
+        {period: 1, team1: "Cleveland Spiders", score1: 35, team2: "Millionare Row", score2: 57},
+        {period: 1, team1: "Social Zeros", score1: 25, team2: "Ironmen", score2:56}
+    ])
+
     const handleSubmit = e => {
         e.preventDefault();
         console.log(team1, team2);
+
+        scoreObj.unshift({period: period, team1: team1, score1: score1, team2: team2, score2: score2});
+        setScore1(0);
+        setScore2(0); 
       }
 
     return (
         <div className="container-fluid">
         <div className='row'>
-            <div className='col-sm'></div>
+            {/* <div className='col-sm'></div> */}
             <div className='col-sm-6'>
             <form onSubmit={handleSubmit}>
-                <div className={'form-group'}>
+            <div className='form-row'>
+                <div className={'form-group col-lg-6'}>
                     <label htmlFor='period'>Period:</label>
                     <input type='text' id='period' className={'form-control'} value={period} onChange={e => setPeriod(e.target.value)} />
                 </div>
+            </div>
                 <div className='form-row'>
                     <div className={'form-group col-lg-6'}>
                     <label htmlFor='team1'>Team 1:</label>
@@ -59,6 +73,14 @@ function RecordEntry() {
             </div>
             <div className='col-sm'></div>
         </div>
+        <div className="row">
+            {/* <div className="col-lg-3"></div> */}
+            <div className="col-lg-6">
+                <DailyScore props = {scoreObj} />
+            </div>
+            {/* <div className="col-lg-3"></div> */}
+        </div>
+        
         </div>
     )
 }
