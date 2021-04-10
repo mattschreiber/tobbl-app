b136b0c9fa8658834b5fc11dc559e70aa3bb5e97
mattschreiber <matt.schreiber01@gmail.com>
2021-04-10 15:07:43 -0400

    added daily score component

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import DailyScore from '../components/DailyScore';
 import Standings from '../components/Standings';
 
 // home page
@@ -7,6 +8,12 @@ function Home() {
         {teamName: 'Naps', teamWins: 5, teamLosses: 2, division: 'Black'}, {teamName: 'Ironmen', teamWins: 6, teamLosses: 1, division: 'Red'}, {teamName: 'Social Zeros', teamWins: 1, teamLosses: 6, division: 'Red'}, {teamName: 'Millionare Row', teamWins: 3, teamLosses: 4, division: 'Red'},
         {teamName: 'Cleveland Spiders', teamWins: 2, teamLosses: 5, division: 'Black'}, {teamName: 'Durham Bulls', teamWins: 6, teamLosses: 1, division: 'Black'}
     ]);
+
+    const [scoreObj] = useState([
+        {period: 1, team1: "Naps", score1: 50, team2: "Durham Bulls", score2: 40},
+        {period: 1, team1: "Cleveland Spiders", score1: 35, team2: "Millionare Row", score2: 57},
+        {period: 1, team1: "Social Zeros", score1: 25, team2: "Ironmen", score2:56}
+    ])
 
     // const [division] = useState('');
 
@@ -19,6 +26,13 @@ function Home() {
             <div className='col-lg'>
                 <Standings props = {teamObj} division = "Red" />
             </div>
+        </div>
+        <div className="row">
+            {/* <div className="col-lg-3"></div> */}
+            <div className="col-lg-6">
+                <DailyScore props = {scoreObj} />
+            </div>
+            {/* <div className="col-lg-3"></div> */}
         </div>
         </div>
     )
