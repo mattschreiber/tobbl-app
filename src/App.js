01064252dd58ca1490eca76171af49764eb6f5ab
mattschreiber <matt.schreiber01@gmail.com>
2021-04-10 08:08:07 -0400

    updated layout so form was wider

--- src/App.js
+++ src/App.js
@@ -14,20 +14,21 @@ import './App.css';
 
 const Nav = () => (
   <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" href="#">Navbar</a>
+      <a className="navbar-brand" href="/">Navbar</a>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
   <div className="collapse navbar-collapse" id="navbarNav">
     <ul className="navbar-nav">
       <li className="nav-item active">
-        {/* <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a> */}
         <Link className="nav-link" to="/">Home <span className="sr-only">(current)</span></Link>
       </li>
       <li className="nav-item">
         <Link className="nav-link" to="/record-entry">Enter Score</Link>
       </li>
-      <li className="nav-item">
+    </ul>
+    <ul className="navbar-nav ml-auto">
+    <li className="nav-item">
         <Link className="nav-link" to="/login">Login</Link>
       </li>
     </ul>
