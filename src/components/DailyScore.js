f7e2c0b14647dbed71b7d7f9f9c2d8e04e36c4ff
mattschreiber <matt.schreiber01@gmail.com>
2021-04-11 08:23:16 -0400

    added card to display scores

--- src/components/DailyScore.js
+++ src/components/DailyScore.js
@@ -4,29 +4,37 @@ function DailyScore (props) {
     return (
         <div className='row mt-5'>
             <div className='col-12'>
-                <h3>Daily Score</h3>
-                <table className='table table-striped'>
-                    <thead>
-                        <tr>
-                            <th scope='col'>Period</th>
-                            <th scope='col'>Team 1</th>
-                            <th scope='col'>Score 1</th>
-                            <th scope='col'>Team 2</th>
-                            <th scope="col">Score 2</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {props.props.map((score, i) =>
-                        <tr key={i}>
-                            <td>{score.period}</td>
-                            <td>{score.team1}</td>
-                            <td>{score.score1}</td>
-                            <td>{score.team2}</td>
-                            <td>{score.score2}</td>
-                        </tr>
-                        )}
-                    </tbody>
-                </table>
+                <div className="card mb-3">
+                    <div className="card-header">
+                        Daily Score Log
+                    </div>
+                    <div className="card-body">
+                        <div className="table-responsive">
+                            <table className='table table-striped'>
+                                <thead>
+                                    <tr>
+                                        <th scope='col'>Period</th>
+                                        <th scope='col'>Team 1</th>
+                                        <th scope='col'>Score 1</th>
+                                        <th scope='col'>Team 2</th>
+                                        <th scope="col">Score 2</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {props.props.map((score, i) =>
+                                    <tr key={i}>
+                                        <td>{score.period}</td>
+                                        <td className={score.team1 > score.team2 ? 'font-weight-bold':undefined }>{score.team1}</td>
+                                        <td className={score.team1 > score.team2 ? 'font-weight-bold':undefined }>{score.score1}</td>
+                                        <td className={score.team2 > score.team1 ? 'font-weight-bold':undefined }>{score.team2}</td>
+                                        <td className={score.team2 > score.team1 ? 'font-weight-bold':undefined }>{score.score2}</td>
+                                    </tr>
+                                    )}
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>  
             </div>
         </div>
     )
